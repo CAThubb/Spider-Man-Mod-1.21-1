0e8ddedc6b18a19c33213781c0da82ad0f0c5cbc
dimoch1c <dimitrypatriarch@gmail.com>
2026-05-10 18:34:41 +0500

    feat: Initial commit
    # Conflicts:
    #	LICENSE
    #	README.md

---
+++ src/main/java/com/dimoch1c/spidermanmod/SpidermanmodClient.java
@@ -0,0 +1,10 @@
+package com.dimoch1c.spidermanmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpidermanmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
