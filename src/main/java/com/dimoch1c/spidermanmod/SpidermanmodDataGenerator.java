a1e54a61dddd077430c3c8d88754c6315f84c4da
dimoch1c <dimitrypatriarch@gmail.com>
2026-05-10 18:58:09 +0500

    feat: Add a DataGen

--- src/main/java/com/dimoch1c/spidermanmod/SpidermanmodDataGenerator.java
+++ src/main/java/com/dimoch1c/spidermanmod/SpidermanmodDataGenerator.java
@@ -1,11 +1,18 @@
 package com.dimoch1c.spidermanmod;
 
+import com.dimoch1c.spidermanmod.datagen.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class SpidermanmodDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+        pack.addProvider(ModBlockTagProvider::new);
+        pack.addProvider(ModItemTagProvider::new);
+        pack.addProvider(ModLootTableProvider::new);
+        pack.addProvider(ModModelProvider::new);
+        pack.addProvider(ModRecipeProvider::new);
 	}
 }
